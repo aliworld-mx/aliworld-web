32c9be7ebb0b4cd5d458af6ac00e3ba34c2660a6
Leonardi Pérez <marcolpr98@gmail.com>
2024-12-03 17:04:46 -0600

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Footer } from "./_components/Footer";
 import { Navigation } from "./_components/Navigation";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Navigation />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
