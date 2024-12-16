d85446cff86d6da4348a07d786e3b75f1f5010d1
Leonardi Pérez <marcolpr98@gmail.com>
2024-12-16 12:13:11 -0600

    Fix height issue

--- app/_components/Expedia/ExpediaFlightsSearchWidget.tsx
+++ app/_components/Expedia/ExpediaFlightsSearchWidget.tsx
@@ -4,7 +4,7 @@ export const ExpediaFlightsSearchWidget = () => (
     <iframe
         src='https://expedia-widget.vercel.app/expedia-vuelo.html'
         width="100%"
-        height={400}
+        height={460}
         frameBorder="0" 
         scrolling="no"
         title="Expedia Vuelos Widget"
--- app/_components/Expedia/ExpediaHotelsSearchWidget.tsx
+++ app/_components/Expedia/ExpediaHotelsSearchWidget.tsx
@@ -4,7 +4,7 @@ export const ExpediaHotelsSearchWidget = () => (
     <iframe
         src='https://expedia-widget.vercel.app/expedia-hotel.html'
         width="100%"
-        height={360}
+        height={460}
         frameBorder="0" 
         scrolling="no"
         title="Expedia Hoteles Widget"
