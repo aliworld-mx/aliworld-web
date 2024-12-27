c7c9180495f8fd30ff45e24187ac2a6a528cb96f
Leonardi Pérez <marcolpr98@gmail.com>
2024-12-27 14:37:28 -0600

    Update route.ts

--- app/api/join-newsletter/route.ts
+++ app/api/join-newsletter/route.ts
@@ -1,8 +1,8 @@
 import Mailjet from 'node-mailjet';
 
 const mailjet = Mailjet.apiConnect(
-    process.env.MAILJET_API_KEY ?? "", // API Key pública de Mailjet
-    process.env.MAILJET_SECRET_KEY ?? "", // API Key privada de Mailjet
+  process.env.MAILJET_API_KEY ?? "", // API Key pública de Mailjet
+  process.env.MAILJET_SECRET_KEY ?? "", // API Key privada de Mailjet
 );
 
 const newsLetterEmail = `<!DOCTYPE html>
@@ -102,29 +102,39 @@ const newsLetterEmail = `<!DOCTYPE html>
 `
 
 export async function POST(request: Request) {
-    const { email } = await request.json();
+  const { email } = await request.json();
 
-    try {
-        // Configuración del correo
-        const requestMail = mailjet
-            .post('send')
-            .request({
-                FromEmail: 'avisos@aliworld.mx',
-                FromName: 'Aliworld',
-                Subject: "¡Bienvenido a Aliworld!",
-                'Html-part': newsLetterEmail,
-                Recipients: [{ Email: email }],
-            });
+  try {
+    // Configuración del correo
+    const requestMail = mailjet
+      .post('send')
+      .request({
+        FromEmail: 'info@aliworld.mx',
+        FromName: 'Aliworld',
+        Subject: "¡Bienvenido a Aliworld!",
+        'Html-part': newsLetterEmail,
+        Recipients: [{ Email: email }],
+      });
 
-        const result = await requestMail;
+    const result = await requestMail;
 
-        return new Response(JSON.stringify(result.body), {
-            status: 200,
-        });
+    mailjet
+      .post('send')
+      .request({
+        FromEmail: 'info@aliworld.mx',
+        FromName: 'Aliworld',
+        Subject: "¡Bienvenido a Aliworld!",
+        Recipients: [{ Email: 'contacto@aliworld.mx' }],
+        'Html-part': `<p>Nuevo suscriptor a la newsletter: ${email}</p>`,
+      });
 
-    } catch (error: any) {
-        return new Response(JSON.stringify({ error: error.message }), {
-            status: 500,
-        });
-    }
+    return new Response(JSON.stringify(result.body), {
+      status: 200,
+    });
+
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
 }
