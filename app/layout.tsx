36df3e26cb7b0d01dbc7fbad0daf6a58a3ab1122
Leonardi Pérez <marcolpr98@gmail.com>
2024-12-29 18:52:24 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,15 @@ export default async function RootLayout({
           ttq.page();
 }(window, document, 'ttq');`}
         </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11526773841">
+        </script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-11526773841');`}
+        </script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
