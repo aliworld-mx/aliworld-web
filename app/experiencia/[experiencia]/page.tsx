4aaded81a5f2d04111fa7657e954ad386cecdc30
Leonardi Pérez <marcolpr98@gmail.com>
2025-02-01 19:38:43 -0600

    Fix redirect for MT

--- app/experiencia/[experiencia]/page.tsx
+++ app/experiencia/[experiencia]/page.tsx
@@ -1,10 +1,10 @@
-import { getTrip } from '@/app/lib/getTrip'
+import { getTripByMT } from '@/app/lib/getTripByMT'
 import { PageProps } from '@/.next/types/app/page'
 import { redirect } from 'next/navigation'
 
 export default async function ExperienciaPage({ params }: PageProps) {
     const { experiencia } = await params;
-    const experience = await getTrip(experiencia);
+    const experience = await getTripByMT(experiencia);
     
     redirect(`/paquetes/${experience.fields.destino.fields.id}/${experience.fields.slug}`);
 }
---
+++ app/lib/getTripByMT.ts
@@ -0,0 +1,21 @@
+import { notFound } from "next/navigation";
+import { contentfulClient } from "../_utils/contentful";
+import { TypePaquete } from "../_types/contentful/Paquete";
+
+export async function getTripByMT(id: string) {
+    try {
+        const response = await contentfulClient.getEntries({
+            content_type: 'paquete',
+            'fields.id': id,
+        });
+
+        if (response.total === 0) {
+            throw new Error('No se encontró el paquete');
+        }
+
+        return response.items[0] as unknown as TypePaquete;
+    } catch (error) {
+        console.error(error);
+        return notFound();
+    }
+}
